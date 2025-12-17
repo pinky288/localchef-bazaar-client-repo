e4a352017af2f53f8ce9877c067b6bff536ae6db
pinky288 <kjpinky345@gmail.com>
2025-12-17 15:52:50 +0600

    add chef dashboard pages

--- src/Layout/ChefDashboardLayout.jsx
+++ src/Layout/ChefDashboardLayout.jsx
@@ -2,33 +2,30 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const ChefDashboardLayout = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+   <div className="flex min-h-screen bg-gray-100">
 
       <aside className="w-64 bg-[#8D0B41] text-white p-6">
         <h2 className="text-2xl font-bold mb-6 text-center">Chef Dashboard</h2>
         <nav className="space-y-3">
-          <NavLink
-            to="profile"
-            className={({ isActive }) =>
+          <NavLink     to="profile" className={({ isActive }) =>
               `block px-4 py-2 rounded ${isActive ? "bg-white text-[#8D0B41]" : "hover:bg-[#FF5C8D]"}`
             }
           >
             My Profile
-          </NavLink>
+       </NavLink>
           <NavLink
-            to="create-meal"
+         to="create-meal"
             className={({ isActive }) =>
               `block px-4 py-2 rounded ${isActive ? "bg-white text-[#8D0B41]" : "hover:bg-[#FF5C8D]"}`
            }
           >
-            Create Meal
+    Create Meal
           </NavLink>
           <NavLink
             to="my-meals"
             className={({ isActive }) =>
               `block px-4 py-2 rounded ${isActive ? "bg-white text-[#8D0B41]" : "hover:bg-[#FF5C8D]"}`
-            }
-          >
+            }>
             My Meals
           </NavLink>
           <NavLink
@@ -41,11 +38,9 @@ const ChefDashboardLayout = () => {
           </NavLink>
         </nav>
       </aside>
-
-   
       <main className="flex-1 p-6">
-        <Outlet />
-      </main>
+      <Outlet />
+   </main>
     </div>
   );
 };
