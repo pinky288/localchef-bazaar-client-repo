c3226c8658b15ede37e86054a70a06aeb925f90f
pinky288 <kjpinky345@gmail.com>
2025-12-17 17:23:14 +0600

    add chef profile

--- src/pages/Dashboard/Chef/ChefProfile.jsx
+++ src/pages/Dashboard/Chef/ChefProfile.jsx
@@ -29,19 +29,20 @@ const ChefProfile = () => {
     return () => unsubscribe();
   }, []);
 
+
+
   if (loading) return <p className="text-center mt-10">Loading profile...</p>;
   if (!chef) return <p className="text-center mt-10">No profile found</p>;
-
   return (
     <div className="p-4 flex justify-center">
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center text-[#8D0B41]">Chef Profile</h2>
-        <div className="space-y-3 text-gray-700">
-          <p><strong>Name:</strong> {chef.name}</p>
-          <p><strong>Email:</strong> {chef.email}</p>
+     <div className="space-y-3 text-gray-700">
+        <p><strong>Name:</strong> {chef.name}</p>
+      <p><strong>Email:</strong> {chef.email}</p>
           <p><strong>Phone:</strong> {chef.phone || "N/A"}</p>
           <p><strong>Address:</strong> {chef.address || "N/A"}</p>
-        </div>
+   </div>
       </div>
     </div>
   );
